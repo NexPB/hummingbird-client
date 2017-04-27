68e276ae628d8723e11a27cffd0bbb7eb17b883b
Thomas McNiven <hello@vevix.net>
2017-04-27 20:29:05 +1000

    Hookup library layout switching

--- app/routes/users/library.js
+++ app/routes/users/library.js
@@ -4,8 +4,6 @@ import set from 'ember-metal/set';
 import service from 'ember-service/inject';
 import { task } from 'ember-concurrency';
 import { storageFor } from 'ember-local-storage';
-import libraryStatus from 'client/utils/library-status';
-import errorMessages from 'client/utils/error-messages';
 import getTitleField from 'client/utils/get-title-field';
 import Pagination from 'client/mixins/pagination';
 
@@ -15,14 +13,18 @@ export default Route.extend(Pagination, {
     status: { refreshModel: true },
     sort: { refreshModel: true }
   },
+
   intl: service(),
-  notify: service(),
-  metrics: service(),
   lastUsed: storageFor('last-used'),
 
+  /**
+   * Use the cached query param values for the user if this is the user's *own* page.
+   * Only override the query param values if they aren't explicitly provided.
+   */
   beforeModel({ queryParams }) {
-    if (queryParams.media === undefined || queryParams.sort === undefined) {
-      if (get(this, 'session').isCurrentUser(this.modelFor('users'))) {
+    if (!queryParams.media || !queryParams.sort) {
+      const isCurrentUser = get(this, 'session').isCurrentUser(this.modelFor('users'));
+      if (isCurrentUser) {
         const lastUsed = get(this, 'lastUsed');
         const { libraryType, librarySort } = getProperties(lastUsed, 'libraryType', 'librarySort');
         if (libraryType || librarySort) {
@@ -39,7 +41,7 @@ export default Route.extend(Pagination, {
 
   model(params) {
     return {
-      taskInstance: get(this, 'queryLibraryEntriesTask').perform(params),
+      taskInstance: get(this, 'getLibraryEntriesTask').perform(params),
       paginatedRecords: []
     };
   },
@@ -57,84 +59,81 @@ export default Route.extend(Pagination, {
 
   /**
    * Restartable task that queries the library entries for the current status,
-   * and media type.
+   * and media type. This is invoked by the model hook.
    */
-  queryLibraryEntriesTask: task(function* (params) {
+  getLibraryEntriesTask: task(function* (params) {
     const options = this._getRequestOptions(params);
     return yield this.queryPaginated('library-entry', options);
   }).restartable(),
 
-  _getUsableSort(sort) {
-    const controller = this.controllerFor(get(this, 'routeName'));
+  /**
+   * Convert the query param `sort` key into a key that the API expects.
+   *
+   * @param {String} Sorting Value
+   */
+  _getSortingKey(sort) {
+    // get the query param value for `media`
+    const routeName = get(this, 'routeName');
+    const controller = this.controllerFor(routeName);
     const mediaType = get(controller, 'media');
-    if (sort === 'type' || sort === '-type') {
-      const field = `${mediaType}.subtype`;
-      return sort.charAt(0) === '-' ? `-${field}` : field;
-    } else if (sort === 'title' || sort === '-title') {
-      let field = `${mediaType}.titles`;
-      if (get(this, 'session.hasUser')) {
-        const preference = get(this, 'session.account.titleLanguagePreference').toLowerCase();
-        const key = getTitleField(preference);
-        field = `${field}.${key}`;
-      } else {
-        field = `${field}.canonical`;
-      }
-      return sort.charAt(0) === '-' ? `-${field}` : field;
-    }
-    return sort;
-  },
 
-  actions: {
-    saveEntry(entry) {
-      if (get(entry, 'validations.isValid') === true) {
-        return entry.save()
-          .then(() => get(this, 'notify').success('Your library entry was updated!'))
-          .catch((err) => {
-            entry.rollbackAttributes();
-            get(this, 'notify').error(errorMessages(err));
-          });
+    // get the correct sorting key
+    switch (sort) {
+      case 'type':
+      case '-type': {
+        const field = `${mediaType}.subtype`;
+        return sort.charAt(0) === '-' ? `-${field}` : field;
       }
-    },
-
-    changeSort({ type, direction }) {
-      const controller = this.controllerFor(get(this, 'routeName'));
-      const sort = direction === 'desc' ? `-${type}` : type;
-      set(controller, 'sort', sort);
+      case 'title':
+      case '-title': {
+        let field = `${mediaType}.titles`;
+        // If the user is logged in, then we want to use their preferred title preference
+        if (get(this, 'session.hasUser')) {
+          const preference = get(this, 'session.account.titleLanguagePreference');
+          const key = getTitleField(preference.toLowerCase());
+          field = `${field}.${key}`;
+        } else {
+          field = `${field}.canonical`;
+        }
+        return sort.charAt(0) === '-' ? `-${field}` : field;
+      }
+      default: {
+        return sort;
+      }
     }
   },
 
+  /**
+   * Build the JSON-API request options for the model hook
+   *
+   * @param {Object} Params
+   */
   _getRequestOptions({ media, status, sort }) {
     const user = this.modelFor('users');
-    const userId = get(user, 'id');
     const options = {};
 
     // apply user sort selection
-    if (sort !== undefined) {
-      Object.assign(options, { sort: this._getUsableSort(sort) });
+    if (sort) {
+      const sortingKey = this._getSortingKey(sort);
+      Object.assign(options, { sort: sortingKey });
+    } else {
+      Object.assign(options, { sort: '-updated_at' });
     }
 
     if (status === 'all') {
       status = '1,2,3,4,5'; // eslint-disable-line no-param-reassign
-      if (sort !== undefined) {
-        Object.assign(options, { sort: ['status', get(options, 'sort')].join(',') });
-      } else {
-        Object.assign(options, { sort: 'status,-updated_at' });
-      }
-    } else {
-      // eslint-disable-next-line no-param-reassign
-      status = libraryStatus.enumToNumber(status);
-      if (sort === undefined) {
-        Object.assign(options, { sort: '-updated_at' });
-      }
+      const sortingKey = `status,${options.sort}`;
+      Object.assign(options, { sort: sortingKey });
     }
 
-    // sparse fieldsets
-    Object.assign(options, this._getFieldsets(media));
+    // request only the fields that we require for display
+    const fields = this._getFieldsets(media);
+    Object.assign(options, { fields });
 
     return Object.assign(options, {
       include: `${media},user`,
       filter: {
-        user_id: userId,
+        user_id: get(user, 'id'),
         kind: media,
         status
       },
@@ -144,24 +143,24 @@ export default Route.extend(Pagination, {
 
   /**
    * Request only the fields that we need for this resource
+   *
+   * @param {String} Media
    */
   _getFieldsets(media) {
     const unitCount = media === 'anime' ? 'episodeCount' : 'chapterCount';
     return {
-      fields: {
-        [media]: [
-          'slug',
-          'posterImage',
-          'canonicalTitle',
-          'titles',
-          'synopsis',
-          'subtype',
-          'startDate',
-          'endDate',
-          unitCount
-        ].join(','),
-        users: 'id'
-      }
+      [media]: [
+        'slug',
+        'posterImage',
+        'canonicalTitle',
+        'titles',
+        'synopsis',
+        'subtype',
+        'startDate',
+        'endDate',
+        unitCount
+      ].join(','),
+      users: 'id'
     };
   }
 });
